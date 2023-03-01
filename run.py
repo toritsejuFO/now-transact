7e3a22e00bc3e5b7469a51e41985aac2c34f61d8
Faith Odonghanro <toriboi.fo@gmail.com>
2023-03-01 17:12:31 +0100

    Setup logging and error handlers in create_app factory

--- run.py
+++ run.py
@@ -1,5 +1,3 @@
-import os
-import logging
 from dotenv import load_dotenv
 from connexion.resolver import RestyResolver
 from flask_migrate import Migrate
@@ -7,16 +5,12 @@ from flask_migrate import Migrate
 load_dotenv()
 
 from app import create_app, db
-from app.exceptions import setup_error_handler
 
 connexion_app = create_app()
 connexion_app.add_api('../api_spec.yml', resolver=RestyResolver('app.controllers'))
 app = connexion_app.app
 
-logging.getLogger(os.getenv('APP_NAME')).setLevel(logging.INFO)
-
 Migrate(app, db)
-setup_error_handler(app)
 
 @app.route('/')
 def home():
